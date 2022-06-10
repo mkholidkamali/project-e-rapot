afbd84f664b955b6b06013fbad074c6a739a75fb
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-06-10 09:23:21 +0700

    UI Nilai

--- resources/views/dashboard/nilai/index.blade.php
+++ resources/views/dashboard/nilai/index.blade.php
@@ -2,5 +2,75 @@
 @extends('layouts.dashboard.main')
 
 @section('main')
-    <h1>Belum dibuat</h1>
+
+    <h1 class="mt-3">Nilai</h1>
+    <hr>
+
+    <div class="card">
+        <div class="card-body">
+
+            <h5>Data Nilai</h5>
+            <small>Note* : Ini nanti mungkin di select dlu baru muncul datanya</small>
+            <div class="col-md-3">
+                <form action="" method="post" class="mt-2 px-2">
+                    <div class="mb-2">
+                        <label for="kelas" class="form-label">Kelas</label>
+                        <select class="form-select" aria-label="Default select example" name="kelas" id="kelas">
+                            <option value="x">X Tel 1</option>
+                            <option value="x">XI Tel 2</option>
+                            <option value="x">XII Tel 3</option>
+                        </select>
+                    </div>
+                    <div class="mb-2">
+                        <label for="mata-pelajaran" class="form-label">Mata Pelajaran</label>
+                        <select name="mata-pelajaran" id="mata-pelajaran" class="form-select">
+                            <option value="x">MTK</option>
+                            <option value="x">Bahasa Indonesia</option>
+                            <option value="x">Pemrograman Web</option>
+                        </select>
+                    </div>
+                    <button class="btn btn-dark mt-2 px-4">Pilih</button>
+                </form>
+            </div>
+
+            <div class="table-responsive mt-3">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr >
+                            <td>#</td>
+                            <td>Nama Siswa</td>
+                            <td>Nilai Harian</td>
+                            <td>UTS</td>
+                            <td>UAS</td>
+                            <td>Pengetahuan</td>
+                            <td style="width: 4vh">Predikat Pengetahuan</td>
+                            <td>Ketrampilan</td>
+                            <td style="width: 4vh">Predikat Ketrampilan</td>
+                            <td>Opsi</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Anthonio D Caprio</td>
+                            <td>89</td>
+                            <td>90</td>
+                            <td>89</td>
+                            <td>89</td>
+                            <td>B</td>
+                            <td>95</td>
+                            <td>A</td>
+                            <td class="text-center">
+                                <button class="btn btn-primary"><i class="bi bi-eye"></i></button>
+                                <button class="btn btn-warning"><i class="bi bi-pencil-square"></i></button>
+                                <button class="btn btn-danger"><i class="bi bi-trash-fill"></i></button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    </div>
+
 @endsection
